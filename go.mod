68e70276ce0bd6f544f6999c79f54aef2e0a43fb
Dinmukhammed <dimash.r53@gmail.com>
2023-10-10 22:27:37 +0600

    pass messages to all subscribes

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module websocket-chat
 
 go 1.20
 
-require github.com/gorilla/websocket v1.5.0
+require (
+	github.com/gorilla/websocket v1.5.0
+	github.com/mgutz/logxi v0.0.0-20161027140823-aebf8a7d67ab
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"sync/atomic"
+
+	log "github.com/mgutz/logxi/v1"
 
 	"github.com/gorilla/websocket"
 )
@@ -14,25 +16,85 @@ var (
 	}
 )
 
-func reader(conn *websocket.Conn) {
-	defer conn.Close()
+type Message struct {
+	Type   int
+	Data   []byte
+	Sender int
+}
+
+/*
+in1  \              / out1
+in2  -->  fanIn  -->  out2
+in3  /              \ out3
+*/
+
+func fanIn(msgs <-chan Message, consumers []chan Message) {
+	for msg := range msgs {
+		log.Info("fanIn", "msg", msg.Data, "cc", len(consumers))
+		for i, out := range consumers {
+			if msg.Sender == i {
+				continue
+			}
+			if out == nil {
+				continue
+			}
+			log.Info("fanIn write to consumer")
+			out <- msg
+		}
+	}
+}
+
+func reader(id int, conn *websocket.Conn, out chan<- Message) {
 	for {
-		log.Printf(">>> reading msg")
-		t, p, err := conn.ReadMessage()
+		t, msg, err := conn.ReadMessage()
 		if err != nil {
 			if t == websocket.CloseMessage {
-				log.Printf("closing connection")
 				return
 			}
-			log.Printf("!!! wtf on read: %v", err)
+			log.Error("error on read msg", "err", err)
 			return
 		}
+		log.Info("read", "msg", msg)
+		out <- Message{t, msg, id}
+	}
+}
 
-		log.Printf("t: %v, p: %v", t, string(p))
+var (
+	defaultRoom = newRoom(10)
+)
 
-		if err := conn.WriteMessage(t, p); err != nil {
-			log.Printf("!!! wtf on write: %v", err)
-			return
+type room struct {
+	msgs   chan Message
+	actors []chan Message
+
+	id int64
+}
+
+func newRoom(capacity int) *room {
+	return &room{
+		actors: make([]chan Message, capacity),
+		msgs:   make(chan Message),
+	}
+}
+
+func (r *room) add(conn *websocket.Conn) {
+	out := make(chan Message)
+	ind := atomic.LoadInt64(&r.id)
+	r.actors[ind] = out
+	atomic.AddInt64(&r.id, 1)
+	go func() {
+		defer conn.Close()
+		// TODO: remove from consumers
+
+		reader(int(ind), conn, r.msgs)
+	}()
+	go writer(conn, out)
+}
+
+func writer(conn *websocket.Conn, msgs <-chan Message) {
+	for m := range msgs {
+		if err := conn.WriteMessage(m.Type, m.Data); err != nil {
+			log.Error("error on write", "err", err)
 		}
 	}
 }
@@ -42,14 +104,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Error("error on upgrade connection", "err", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
-	reader(ws)
+	defaultRoom.add(ws)
 }
 
 func main() {
+	go fanIn(defaultRoom.msgs, defaultRoom.actors)
 	http.HandleFunc("/", handler)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	h := ":8080"
+	log.Info("starting web server", "host", h)
+	if err := http.ListenAndServe(h, nil); err != nil {
+		log.Fatal(err.Error())
+	}
 }
